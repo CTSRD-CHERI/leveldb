8d97489e453b009e6688e471a8a1348c1bf254db
Hongyan Xia <Jerryxia32@gmail.com>
2018-02-20 14:21:39 +0000

    Fix bugs in the run script.

--- run.sh
+++ run.sh
@@ -1,15 +1,16 @@
-#!/bin/ls
+#!/bin/sh
 
 NUM=100000
 
-BENCHMARKS=fillseq fillrandom fillrandbatch overwrite overwritebatch readrandom
+BENCHMARKS="fillseq fillrandom fillrandbatch overwrite overwritebatch readrandom"
 
 RUNS=2
 
 for i in $BENCHMARKS; do
+  echo "Running $i benchmark."
   ln -s db_bench_sqlite3 $i
   for j in $(seq 1 $RUNS); do
-    ./i --benchmarks=$i --num=$NUM
+    ./$i --benchmarks=$i --num=$NUM
   done
   rm $i
 done
